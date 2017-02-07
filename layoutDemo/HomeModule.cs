09a1a36e156eea538e08d824e9f09811ab94367f
Matthew Caswell and Sam Kirsch <matthew+sam@>
2017-02-07 11:01:06 -0800

    move all practice work into one file to push

---
+++ layoutDemo/HomeModule.cs
@@ -0,0 +1,16 @@
+using Nancy;
+
+namespace FriendLetter
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ =>
+      {
+        return View["index.cshtml"];
+      };
+
+    }
+  }
+}
